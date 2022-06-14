3767105ea49827015e0af0766ece4dd3aaee757a
chatchai <chatchai.sirithipvanith@gmail.com>
2022-06-14 18:22:05 +0700

    quite good protocol for communication with subprocess, however, if this method would suck if the process is not respond to available commands

--- rlcraft.py
+++ rlcraft.py
@@ -1,82 +1,88 @@
 import asyncio
 from subprocess import Popen, PIPE, STDOUT
 from threading import Thread, Lock
-from multiprocessing import Process, Queue
+from multiprocessing.connection import Connection
+from multiprocessing import Process, Queue, Pipe
 import logging
+import time
+import io
 
 
-class FQueue:
-    def __init__(self, *args, **kwargs):
-        self.q = Queue(*args, **kwargs)
-
-    def flush(self):
-        q = []
-        while not self.empty():
-            q.append(self.q.get())
-        return q
-
-    def put(self, item, *args, **kwargs):
-        return self.q.put(item, *args, **kwargs)
-
-    def get(self, *args, **kwargs):
-        return self.q.get(*args, **kwargs)
-
-    def full(self, *args, **kwargs):
-        return self.q.full(*args, **kwargs)
-        
-    def empty(self, *args, **kwargs):
-        return self.q.empty(*args, **kwargs)
 
 
-class LineBusket(FQueue):
-    def put(self, item, *args, **kwargs):
-        # validate if item is str
-        assert type(item) is str, "item is expected to be string"
-        super().put(item, *args, **kwargs)
-
-    def flush(self):
-        q = []
-        while not self.empty():
-            line = self.get()
-            q.append(line)
-            print(line, end='')
-        return q
-
-
-def flush_until_block(stdout, break_sig, line_busket):
+def flush_until_block(stdout: io.BufferedReader, pipe: Connection):
+    logger = logging.getLogger("flush_until_block")
     while True:
-        line = stdout.readline()
-        line_busket.put(line)
+        line = stdout.readline() # block line release when stdin
+        pipe.send(line) # only send each lines
         # print(f"{line} length: {len(line)}", end="")
-        if not line:
-            # print(f"{line} length: {len(line)}, bool: {bool(line)}", end="")
-            break
-    break_sig.put(False)
+        busy = pipe.poll() # check if there is something sent ?
+        if busy:
+            logger.info("reader: get something from pipe")
+            terminate = pipe.recv() # get terminate signal
+            logger.info("reader: it is terminate signal")
+            if terminate: 
+                break
 
 
-def wait_until_input_availabel(process: Popen, method=flush_until_block):
+def wait_until_input_availabel(process: Popen, wait_for: float = 5.0, get_return: bool=False) -> list:
     """
         stdin as _io.TextIOWarpper, 
         stdout as _io.BufferedReader
         return remaining line that is not flushed
     """
-    nexit=True
-    break_sig = Queue(maxsize=1)
-    line_busket = LineBusket(maxsize=30)
-    p = Process(target=method, args=(process.stdout, break_sig, line_busket))
+    line_busket = []
+    pipe, pipe_client = Pipe()
+    p = Process(target=flush_until_block, args=(process.stdout, pipe_client))
     logging.info("start listening to stdout")
     p.start()
-    while nexit:
-        process.stdin.write("\n")
-        process.stdin.flush()
-        p.join(timeout=1)
-        if line_busket.full():
-            line_busket.flush()
-        if break_sig.full():
-            nexit = break_sig.get()
-    return line_busket.flush()
-            
+
+    def recv():
+        line = pipe.recv()
+        print(line, end="")
+        if not get_return:
+            line_busket.append(line)
+
+    while True:
+        # p.join(timeout=1)
+        busy = pipe.poll(timeout=wait_for) #
+        if not busy:
+            logging.info("process stop reporting for 5 seconds, try sending input to see if the process response")
+            # poking to see if the subprocess is responding to input
+            process.stdin.write("\n")
+            process.stdin.flush()
+            busy = pipe.poll(timeout=0.2) # wait for 200 ms
+            if busy: # means the process is ready to recieve input
+                logging.info("the process responded, it is ready for input")
+                # logging.info("try sending terminate signal to reader")
+                logging.info("try to terminate process")
+                while p.is_alive():
+                    logging.info("sending terminate signal")
+                    while pipe.poll(timeout=0.2):
+                        recv()
+                    pipe.send(True) # send terminate signal
+                    # poke the subprocess again to release the process from 
+                    # blocking readline() and recieve the terminate signal
+                    process.stdin.write("\n")
+                    process.stdin.flush()
+                    time.sleep(0.2) # if not wait it is slipping
+                break
+        
+        recv()
     
+    p.close()
+    pipe.close()
+    pipe_client.close()
+    logging.info("closing all connections and terminate reader process")
+    return line_busket
+            
+
+def communicate(process: Popen, process_cmd: str, wait_for=0.2):
+    logging.info(f"write to stdin stream: {process_cmd}")
+    process.stdin.write(f"{process_cmd}\n")
+    process.stdin.flush()
+    return wait_until_input_availabel(process, get_return=True)
+
 
 class MinecraftServer:
 
@@ -90,10 +96,11 @@ class MinecraftServer:
         exit_code = self.process.poll()
         if exit_code is not None:
             return exit_code
-        self.process.stdin.reconfigure(line_buffering=True) # flush buffer when \n is written
+        # self.process.stdin.reconfigure(line_buffering=True) # flush buffer when \n is written
         wait_until_input_availabel(self.process)
         self.status = True
 
-    def send_command(self, server_command: str):
+    def send_command(self, server_cmd: str):
         # command validation
+        communicate(self.process, server_cmd)
         pass
